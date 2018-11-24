b24fab2a990c2e48b9ddde8f70bfcb42e55e502a
Johan van Overbeeke <anarril@xs4all.nl>
2018-11-25 00:28:19 +0100

    Added debug code

--- install-jenkins-plugins.sh
+++ install-jenkins-plugins.sh
@@ -4,14 +4,17 @@ echo "Downloading plugins"
 for plugin in "$@"
 do
 	echo "  downloading $plugin"
-	curl -s -L https://updates.jenkins-ci.org/latest/ansicolor.hpi -o /tmp/WEB-INF/plugins/$plugin.hpi
+	curl -L https://updates.jenkins-ci.org/latest/ansicolor.hpi -o /tmp/WEB-INF/plugins/$plugin.hpi
 done
 
-cd /tmp;
+ls -l /tmp/WEB-INF/plugins/
+
+cd /tmp
 echo "Adding plugins"
 
 for plugin in "$@"
 do
 	zip --grow /usr/share/jenkins/jenkins.war WEB-INF/plugins/$plugin.hpi
+	ls -l /usr/share/jenkins/jenkins.war
 done
 
